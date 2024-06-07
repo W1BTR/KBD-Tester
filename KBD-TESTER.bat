5a0907619a67db4656cea2b2cabf5dcc7250e86f
Lucas Elliott W1BTR <lucas.elliott@modularcommunications.com>
2024-06-07 16:02:24 -0400

    Update KBD-TESTER.bat

--- KBD-TESTER.bat
+++ KBD-TESTER.bat
@@ -1,10 +1,15 @@
 @echo off
 title KBD
 mode con: cols=25 lines=5
+echo Tester by W1BTR.
+echo Press any key...
+timeout /t 1 >nul
 cls
 call kbd.exe
 :disp
 cls
-echo You Pressed: %errorlevel%
+echo You pressed a key at
+echo %time%. Woohoo!
+echo kbd.exe reply: [96m%errorlevel%[0m
 call kbd.exe
 goto disp
